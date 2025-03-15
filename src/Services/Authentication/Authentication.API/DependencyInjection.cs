b5229627d3fdcd9b9bc241735f2cc0a766479e2c
MichalZdanuk <zdanuk_m@wp.pl>
2025-03-15 21:47:04 +0100

    Extract MediatR registration to Shared project.

--- src/Services/Authentication/Authentication.API/DependencyInjection.cs
+++ src/Services/Authentication/Authentication.API/DependencyInjection.cs
@@ -1,5 +1,5 @@
 ﻿using Authentication.API.Services;
-using Shared.Behaviors;
+using Shared.DependencyInjection;
 using System.Reflection;
 
 namespace Authentication.API;
@@ -8,11 +8,7 @@ public static class DependencyInjection
 {
 	public static IServiceCollection AddServices(this IServiceCollection services)
 	{
-		services.AddMediatR(config =>
-		{
-			config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-			config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-		});
+		services.AddApplicationServices(Assembly.GetExecutingAssembly());
 
 		services.AddScoped<ICustomAuthService, CustomAuthService>();
 
---
+++ src/Shared/Shared/DependencyInjection/ApplicationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shared.Behaviors;
+using System.Reflection;
+
+namespace Shared.DependencyInjection;
+public static class ApplicationExtensions
+{
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services, Assembly assembly)
+	{
+		services.AddMediatR(config =>
+		{
+			config.RegisterServicesFromAssembly(assembly);
+			config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+		});
+
+		return services;
+	}
+}
