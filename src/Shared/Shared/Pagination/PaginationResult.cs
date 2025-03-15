e812a045b80993777c6b4704ca07b484c6bae60d
MichalZdanuk <zdanuk_m@wp.pl>
2025-03-15 20:24:42 +0100

    Implements pagination models in Shared project.

---
+++ src/Shared/Shared/Pagination/PaginationResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Pagination;
+public class PaginationResult<TModel>(int pageIndex,
+	int pageSize,
+	int count,
+	IEnumerable<TModel> data)
+	where TModel : class
+{
+	public int PageIndex { get; } = pageIndex;
+	public int PageSize { get; } = pageSize;
+	public int Count { get; } = count;
+	public IEnumerable<TModel> Data { get; } = data;
+}
