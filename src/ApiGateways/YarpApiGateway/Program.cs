d00dc46d9997329472d034a6501e55e99088698b
MichalZdanuk <zdanuk_m@wp.pl>
2025-03-16 15:34:46 +0100

    Implement routing for ApiGateway.

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,4 +1,40 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(options =>
+	{
+		options.Authority = jwtSettings["Authority"];
+		options.Audience = jwtSettings["Audience"];
+		options.RequireHttpsMetadata = false;
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuer = false,
+			ValidateAudience = true,
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]!)),
+		};
+	});
+
+builder.Services.AddAuthorization(options =>
+{
+	options.AddPolicy("AuthenticatedUsers", policy =>
+		policy.RequireAuthenticatedUser());
+});
+
+builder.Services.AddReverseProxy()
+	.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapReverseProxy();
 
 app.Run();
