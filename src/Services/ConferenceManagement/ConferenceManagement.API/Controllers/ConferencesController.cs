cb129f04e41e7ef508fc746cad4801050cf94465
MichalZdanuk <zdanuk_m@wp.pl>
2025-03-17 20:39:31 +0100

    Initialize Conferences.API project.

---
+++ src/Services/ConferenceManagement/ConferenceManagement.API/Controllers/ConferencesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConferenceManagement.API.Controllers;
+
+[Route("conferences")]
+[ApiController]
+public class ConferencesController : ControllerBase
+{
+	[HttpGet]
+	public async Task<IActionResult> Get()
+	{
+		return Ok("ok");
+	}
+}
