97385c3951ed082759a73449527fe6879aa6421b
MichalZdanuk <zdanuk_m@wp.pl>
2025-03-18 11:10:50 +0100

    Implement abstractions for messaging.

---
+++ src/Shared/Shared.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Messaging.Events;
+public record IntegrationEvent
+{
+	public Guid Id => Guid.NewGuid();
+	public DateTime OccuredAt => DateTime.UtcNow;
+	public string EventType => GetType().AssemblyQualifiedName;
+}
