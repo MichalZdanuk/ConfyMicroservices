f35fa8390cff761e29f38b59ac8de18b2791faa4
MichalZdanuk <zdanuk_m@wp.pl>
2025-03-18 20:15:58 +0100

    Initialize Notification.API project.

---
+++ src/Services/Notification/Notification.API/Controllers/NotificationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Notification.API.Controllers;
+
+[Route("notifications")]
+[ApiController]
+public class NotificationController
+	: ControllerBase
+{
+	[HttpGet("test")]
+	public async Task<IActionResult> Get()
+	{
+		return Ok("ok");
+	}
+}
---
+++ src/Services/Notification/Notification.API/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Shared.Exceptions.Handler;
+
+namespace Notification.API;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddExceptionHandler<GlobalExceptionHandler>();
+
+		return services;
+	}
+
+	public static WebApplication UseApiServices(this WebApplication app)
+	{
+		app.UseExceptionHandler(options => { });
+
+		return app;
+	}
+}
