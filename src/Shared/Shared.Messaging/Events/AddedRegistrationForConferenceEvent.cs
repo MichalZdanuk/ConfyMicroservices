52d96110b93dfa898f987df42e60c28f0f847426
MichalZdanuk <zdanuk_m@wp.pl>
2025-04-01 14:48:43 +0200

    Introduce Event contracts for notifications.

---
+++ src/Shared/Shared.Messaging/Events/AddedRegistrationForConferenceEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Messaging.Events;
+public record AddedRegistrationForConferenceEvent : IntegrationEvent
+{
+	public Guid ConferenceId { get; set; }
+	public Guid UserId { get; set; }
+	public string ConferenceName { get; set; } = default!;
+}
